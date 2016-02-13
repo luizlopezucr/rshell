6ac36965ba1c44523231d7d78c65a15473c92c67
Krissi Mena <kmena003@ucr.edu>
2016-02-12 23:47:57 -0800

    fixed exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -4,20 +4,18 @@
 #below says read until my defined string
 ../bin/rshell << EOF
 
+ls || echo noprint&&echo stillno; echo yes  
 ls -a -r; echo hello ||mkdir testing && echo no
 exit
-ls || echo noprint&&echo stillno; echo yes  
 
 EOF
 ../bin/rshell << STOP
-exit
 ls -a && echo true &&  rmdir fakename|| mkdir testing; echo end
+exit
 STOP
-
 ../bin/rshell << ENDME
 ls -r ; rmdir testing
 echo hello || echo nothing
 echo ENDING && echo NOW
 exit
 ENDME
-exit 
