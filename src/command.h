aa99e7e0539084f6e4dc39d95cf2399a58c88404
Krissi Mena <kmena003@ucr.edu>
2016-02-28 12:11:06 -0800

    Fixed parser to account for differing connectors. I.E. &&(

--- src/command.h
+++ src/command.h
@@ -3,7 +3,7 @@
 class COMMAND:public RSHELL{
   public:
    vector<char *>cmd_run;
-   bool missing_para;
+   bool input_err;
    COMMAND():RSHELL(){}
    //reset the vector
    void wipe()
@@ -40,21 +40,24 @@ class COMMAND:public RSHELL{
 	  //variable to keep track of parentheses matching
 	  int depth = 0;
 	  //instantiate the bool to false initially
-	  bool missing_para = false;
+	  bool input_err  = false;
+
+      string con[] = { ";","&&","||", "(", ")"};
 	  //while there are still commands to be read
 	  for(unsigned i = 0; i < cmd_run.size(); ++i)
 		{
 		    //store each token string in a temp
 			string tmp = cmd_run.at(i);
 			//an array of connectors to check for
-			string con[] = { ";","&&","||", "(", ")"};
+			bool con_found = false;
 		    //check the strings for these connectors
 			for(unsigned j = 0; j < 5 ; ++j)
 			{	
 				int pos = tmp.find(con[j]);
 				//if there is a connector for the whole length of the string
-				if(pos != string::npos && tmp != con[j])
+				if(pos != string::npos && tmp != con[j] && !con_found)
 				{		
+						con_found = true;
 						//store the connector in the current position	
 						cmd_run.at(i) = strdup(con[j].c_str());
 						//if there is more after connector
@@ -76,12 +79,14 @@ class COMMAND:public RSHELL{
 			}
 			
 		}
-		//checks for mismatched parentheses
 		for(unsigned i = 0; i < cmd_run.size(); i++)
 		{
+				//checks for mismatched parentheses
 				string temp = cmd_run[i];
 				if(temp == "(" || temp  == ")")
+				{
 						temp  == "(" ? depth++ : depth--;
+				}
 		}
 		//if mismatched then output an error and set the bool variable accordingly
 		if(depth != 0)
@@ -91,10 +96,9 @@ class COMMAND:public RSHELL{
 				//output string error to stderr
 				perror("ERROR: MISSING PARENTHESES");
 				//set bool accordingly
-				missing_para = true;
+				input_err = true;
 		}
    }
-   
 };
 
 
