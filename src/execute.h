a14e8038c358baf488b2e71d04ed6b35c5fd2a2a
Krissi Mena <kmena003@ucr.edu>
2016-03-05 17:04:59 -0800

    Handles nested () after operator

--- src/execute.h
+++ src/execute.h
@@ -7,9 +7,10 @@ class EXECUTE: public COMMAND{
 		bool good_exc;
 		bool close_paren;
 		bool open_paren;
+		bool overrider;
 	public:
 		//default constructor
-		EXECUTE():COMMAND(){good_exc=true;close_paren = false;open_paren = false;}
+		EXECUTE():COMMAND(){good_exc=true;close_paren = open_paren = overrider = false;}
 		void execute(vector<char *> tmp)
 		{
 		    //set size of he vector to size + 1 to account for the null character
@@ -71,7 +72,6 @@ class EXECUTE: public COMMAND{
 			//keeps track of whether "&&" or "||" has been found.
 			bool fa = false;
 			bool fo = false;
-				
 			
 			for(int i = start_index; i < cmd_run.size(); i++ )
 		    {
@@ -88,17 +88,16 @@ class EXECUTE: public COMMAND{
 				{
 				    //executes the last string of commands based on the last
 				    //logical operators
-				    if(fa && good_exc)execute(buff);
-				    else if(fo && !good_exc)execute(buff);
-				    else if(!fa && !fo)execute(buff);
-				    //if does not meet conditions good_exc is false
-				    else if(!fo)
-						good_exc = false;
-				    buff.clear();
+				    if(!overrider)
+				    {
+						if(fa && good_exc)execute(buff);
+						else if(fo && !good_exc)execute(buff);
+						else if(!fa && !fo)execute(buff);
+						buff.clear();
+				    }
 				    open_paren = false;
 				    close_paren = true;
 					left_off = ++i;
-				
 					return;
 				}
 				//if open paren go down a stack level
@@ -106,90 +105,99 @@ class EXECUTE: public COMMAND{
 				{
 						close_paren = false;
 						open_paren = true;
+						//before recursing see if override needs to be set
+						if((fo && good_exc) || (fa && !good_exc))
+								overrider = true;
 						exec_main(++i, left_off);
+						//after recursing back
+						if(overrider)
+								overrider = false;
 				}
 				else
 				{		
-						//ifs to check for connectors 
-						if(cmd  == ";")
+						//if overrider is not true then execute normally otherwise do not do anything at all
+						if(!overrider)
 						{
-							//checks to see how it should execute the commands before it
-							if(fa && good_exc && !close_paren)execute(buff);
-							if(fo && !good_exc && !close_paren)execute(buff);
-							if(!fa && !fo && !close_paren)execute(buff);
-							//reset the values
-							fa = false;
-							fo = false;
-							close_paren = open_paren =  false;
-							//clear out the string we just executed
-							buff.clear();
-						}
-						else if(cmd == "&&")
-						{
-							//if there was an || and the previous command didn't succeed then execute
-							//if there was an && and the previous command worked then execute
-							//takes care of this being the first op
+								//ifs to check for connectors 
+								if(cmd  == ";")
+								{
+										//checks to see how it should execute the commands before it
+										if(fa && good_exc && !close_paren)execute(buff);
+										if(fo && !good_exc && !close_paren)execute(buff);
+										if(!fa && !fo && !close_paren)execute(buff);
+										//there was a close paren and do no work since it has already been done		
+										//clear out the string we just executed
+										buff.clear();
+							
+										//reset the values
+										fa = false;
+										fo = false;
+										close_paren = open_paren =  false;
+								}
+								else if(cmd == "&&")
+								{
+										//if there was an || and the previous command didn't succeed then execute
+										//if there was an && and the previous command worked then execute
+										//takes care of this being the first op
 						
-							if((fa && good_exc || fo && !good_exc || !fa && !fo) && !close_paren )
-							{
-								execute(buff);
-								buff.clear();
-							}
+										if((fa && good_exc || fo && !good_exc || !fa && !fo) && !close_paren )
+										{	
+												execute(buff);
+												buff.clear();
+										}	
 				    
-							//if not meant to execute then do this 
-							//and set the value to false
-							else if((fa && !good_exc || fo && good_exc) && !close_paren)
-							{
-								good_exc = false;
+										//if not meant to execute then do this 
+										//and set the value to false
+										else if((fa && !good_exc || fo && good_exc) && !close_paren)
+										{
+												good_exc = false;
+												buff.clear();
+										}
+										//close paren is true do nothing since the work has already been done
+										//clear what we just executed
+										buff.clear();			
+										//set the bool values of the operator we encountered
+										fa = true;
+										fo = false;
+										close_paren = open_paren = false;
+								}	
+								else if(cmd == "||")
+								{
+										//if there was an && and the previous command succeeded
+										//if there was an ||  and the previous command failed execute
+										//takes care of this being the first op
+										if((fo && !good_exc || fa && good_exc || !fo && !fa) && !close_paren) 
+										{
+												execute(buff);
+												buff.clear();
+										}
+										//if not the base case then don't execute  if there was an || and the exec succeeded
+										else if((fo && good_exc || fa && !good_exc) && !close_paren)
+										{	
+												buff.clear();
+										}
+								//close paren is true do nothing since the work has already been done
+								//clear out what we just executed
 								buff.clear();
-							}
-							//close paren is true do nothing since the work has already been done
-							else 
-						    {
-								buff.clear();			
-						    }
-							//set the bool values of the operator we encountered
-							fa = true;
-							fo = false;
-							close_paren = open_paren = false;
-						}
-						else if(cmd == "||")
-						{
-							//if there was an && and the previous command succeeded
-							//if there was an ||  and the previous command failed execute
-							//takes care of this being the first op
-						    if((fo && !good_exc || fa && good_exc || !fo && !fa) && !close_paren) 
-						    {
-								execute(buff);
-								buff.clear();
-						    }
-						    //if not the base case then don't execute  if there was an || and the exec succeeded
-						    else if((fo && good_exc || fa && !good_exc) && !close_paren)
-						    {	
-								buff.clear();
-						    }
-						    //close paren is true do nothing since the work has already been done
-						    else
-						    {
-						    		 buff.clear();
-						    }
-						    //think about what to put in here
-							//set the bool values of the operator we encountered
-							fo = true;
-							fa = false;
-							close_paren = open_paren =  false;
-						}
-						else
-						{
-							close_paren = open_paren = false;
-							//if no connectors push the string
-							buff.push_back(cmd_run.at(i));
+								//think about what to put in here
+								//set the bool values of the operator we encountered
+								fo = true;
+								fa = false;
+								close_paren = open_paren =  false;
+								}
+								else
+								{	
+										close_paren = open_paren = false;
+										//if no connectors push the string
+										buff.push_back(cmd_run.at(i));
+								}
 						}
 				}
 		    }
 		    if(fa && good_exc)execute(buff);
 		    if(fo && !good_exc)execute(buff);
 		    if(!fa && !fo)execute(buff);
+		    //clear out what we just executed
 		    buff.clear();
 			
 		}
