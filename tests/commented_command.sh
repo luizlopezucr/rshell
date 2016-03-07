9c1c179913697a4c89c57ea99146ea1c64165a68
Krissi Mena <kmena003@ucr.edu>
2016-03-06 16:41:22 -0800

    added tests for precedence, updated commented_command tests

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -2,11 +2,13 @@
 #checking before/after commander, beginning, and end, with a space and no spspace
 #below says read until my defined string
 ../bin/rshell << EOF
-
 #ls -a -r; echo hello || mkdir testing && echo no
 ls -a && #after a (&&) echo true &&  rmdir fakename || mkdir testing; echo end
 ls ||# 
 ls || echo noprint#before a (&&) echo stillno; echo yes  
 echo hello #nothing at the end
+(( echo a ) # && echo b || echo hello)
+# ((echo a && echo b) || echo c) && ((echo d || echo e) && echo f)
+((echo a && echo b) || echo c) && ((echo d || echo e) && echo f)#nothing here either
 exit
 EOF
---
+++ tests/precedence_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#below says read until my defined string
+../bin/rshell << EOF
+(ls -j && echo hi) || echo bye
+(()) && echo hello
+(( echo hi && echo hello) || ( echo foo && echo bar )) 
+((echo hi && echo hello) || ( ( echo foo && echo bar)  && echo a ))
+((echo a && echo b )
+((echo a && echo b) || echo c) && ((echo d || echo e) && echo hi)
+((ls -j && echo foo) || echo hi) || ((echo bar || echo hello) && hello)
+exit
+EOF
