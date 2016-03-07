046fa81201af9956d803a0e0ce5952540b0b9a03
John Doe <luiz.lopezm@gmail.com>
2016-03-06 22:32:04 -0800

    added test functionality to test

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -2,6 +2,8 @@
 #below says read until my defined string
 ../bin/rshell << EOF
 (ls -j && echo hi) || echo bye
+( [ -e /bin/ ] && echo hi) || echo bye
+( [ -f /bin/ ] && echo hi) || echo bye
 (()) && echo hello
 (( echo hi && echo hello) || ( echo foo && echo bar )) 
 ((echo hi && echo hello) || ( ( echo foo && echo bar)  && echo a ))
